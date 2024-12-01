342c388fe26877073afc3db0cc3fa39ff8df5f25
Yohan Maury <mauryyohan1@gmail.com>
2024-12-02 00:30:31 +0100

    Utilisation d'un fichier utilitaire utils.js - DRY

--- day_01/part_one/solution.js
+++ day_01/part_one/solution.js
@@ -1,28 +1,17 @@
+import { readFileContent, extractsSides } from '../utils.js';
 
-const fs = require('fs');
-const leftSideList = [];
-const rightSideList = [];
-const oneListBeforeResult = [];
-
-const readFileContent = (filePath) => {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const lines = fileContent.trim().split('\r\n');
-    const numbers = lines.map((line) => {
-        return line.split(' ')
-    });
-    return numbers;
+const sortList = (list) => {
+    return list.sort((a, b) => a - b);
 }
 
 
+const oneListBeforeResult = [];
 const numbers = readFileContent('input/data.txt');
 
-for (const number of numbers) {
-    leftSideList.push(number.at(0));
-    rightSideList.push(number.at(-1));
-}
+const { leftSideList, rightSideList } = extractsSides(numbers);
 
-const leftSorted = leftSideList.sort((a, b) => a - b);
-const rightSorted = rightSideList.sort((a, b) => a - b);
+const leftSorted = sortList(leftSideList);
+const rightSorted = sortList(rightSideList);
 
 for (let i = 0; i < leftSorted.length; i++) {
     oneListBeforeResult.push(Math.abs(leftSorted[i] - rightSorted[i]));
