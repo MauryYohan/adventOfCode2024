f4552f264196dd28e334bb6fdd23996a3ec0f858
Yohan Maury <mauryyohan1@gmail.com>
2024-12-02 00:30:44 +0100

    Utilisation d'un fichier utilitaire utils.js - DRY

--- day_01/part_two/solution.js
+++ day_01/part_two/solution.js
@@ -1,27 +1,10 @@
+import { readFileContent, extractsSides } from '../utils.js';
 
-const fs = require('fs');
-const leftSideList = [];
-const rightSideList = [];
+
 const oneListBeforeResult = [];
-
-const readFileContent = (filePath) => {
-    const fileContent = fs.readFileSync(filePath, 'utf8');
-    const lines = fileContent.trim().split('\r\n');
-    const numbers = lines.map((line) => {
-        return line.split(' ')
-    });
-    return numbers;
-}
-
-
 const numbers = readFileContent('input/data.txt');
 
-for (const number of numbers) {
-    leftSideList.push(number.at(0));
-    rightSideList.push(number.at(-1));
-}
-
-// The code up to this point is the same as the one in adventOfCode2024/day_01/part_one/solution.js
+const { leftSideList, rightSideList } = extractsSides(numbers);
 
 for (let i = 0; i < leftSideList.length; i++) {
     const num = leftSideList[i];
